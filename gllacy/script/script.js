bd687f0b8fd150f933f1cd8d07287d0f10cf1e2a
oliktva <oliktva@gmail.com>
2016-05-18 16:48:30 +0300

    map edit

--- gllacy/script/script.js
+++ gllacy/script/script.js
@@ -5,7 +5,7 @@ YMaps.jQuery(function () {
 
             // Устанавливает начальные параметры отображения карты: центр карты и коэффициент масштабирования
             map.setCenter(new YMaps.GeoPoint(30.329556174652065,59.939524399400774), 16);
-            
+
             //Добавляем метку
             //Стиль метки
             // Создает стиль
@@ -15,9 +15,12 @@ YMaps.jQuery(function () {
             s.iconStyle.href = "img/pin.png";
             s.iconStyle.size = new YMaps.Point(218, 142);
             s.iconStyle.offset = new YMaps.Point(-40, -138);
+
             // Создает метку
             var placemark = new YMaps.Placemark(new YMaps.GeoPoint(30.323054499999976,59.93863075783236), {style: s});
-
+            // Устанавливает содержимое балуна
+            placemark.name = "Gllacy";
+            placemark.description = "Ваш любимый магазин мороженного!";
             // Добавляет метку на карту
             map.addOverlay(placemark);
         })
