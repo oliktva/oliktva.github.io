29a3e48204655dded676fa252c2070c37eda05f2
Olga Kotova <oliktva@gmail.com>
2016-12-13 06:50:12 +0300

    fix pin

--- mishka/js/script.js
+++ mishka/js/script.js
@@ -26,7 +26,7 @@
 
       var s = new YMaps.Style();
       s.iconStyle = new YMaps.IconStyle();
-      s.iconStyle.href = "../img/icon-map-pin.svg";
+      s.iconStyle.href = "../mishka/img/icon-map-pin.svg";
       s.iconStyle.size = new YMaps.Point(66, 100);
       s.iconStyle.offset = new YMaps.Point(-15, -90);
       var placemark = new YMaps.Placemark(new YMaps.GeoPoint(30.3211, 59.9363), {style: s});
